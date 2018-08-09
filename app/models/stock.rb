e81ae839a2e190b3548359fb066e43650094a70b
game1095 <taweesak.in@gmail.com>
2018-08-09 20:31:52 +0700

    setup and improve design with inspiania

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,6 @@
 class Stock < ApplicationRecord
-    has_many :card
-
-    def stock_name_wtih_amount
-        "แผนก #{self.name} | จำนวนคงเหลือ #{self.amount} ฉบับ"
-     end
-end 
+  has_many :card
+  def stock_name_wtih_amount
+      "แผนก #{self.name} | จำนวนคงเหลือ #{self.amount} ฉบับ"
+  end
+end
